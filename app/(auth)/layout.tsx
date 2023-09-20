5e7c8983f8a08ad4e8fac3a08431c048e693d55f
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-09-20 13:40:04 +0530

    Completion

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className='flex items-center justify-center'>
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout
